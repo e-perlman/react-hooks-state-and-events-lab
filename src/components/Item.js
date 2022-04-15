0f1deb58481cba1fe7d8364875460e12ada07708
Emma Perlman <emmajperlman@gmail.com>
2022-04-15 13:27:44 -0700

    commit tree 7f3862dbc3b79b077920da57cd88a6b29e9cef2d

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [itemStatus,setItemStatus]=useState(false)
+  const cart=itemStatus? "in-cart":''
+  const text=itemStatus? "Remove From Cart":'Add to Cart'
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={()=>setItemStatus(!itemStatus)} className="add">{text}</button>
     </li>
   );
 }
